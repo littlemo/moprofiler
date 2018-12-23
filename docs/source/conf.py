d714dce0c3cea950ff2a654196d2f0fcf4119b64
Moore.Huang <moore@moorehy.com>
2018-12-23 21:42:09 +0800

    Modify: 调整文档生成配置

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 #
 # import os
 # import sys
+import sphinx_rtd_theme
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -73,7 +74,7 @@ language = u'zh_CN'
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -81,7 +82,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
