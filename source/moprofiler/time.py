8e296a28b7e541a51bdbb806e9a952514deaac0d
Moore.Huang <moore@moorehy.com>
2018-12-30 16:23:26 +0800

    Feature: 实现时间分析器的 Mixin 类及相关的代理函数

--- source/moprofiler/time.py
+++ source/moprofiler/time.py
@@ -3,10 +3,13 @@
 提供用于时间性能分析的工具
 """
 import logging
+from collections import defaultdict
+from contextlib import contextmanager
 from functools import wraps
 from time import time
 
 from line_profiler import LineProfiler
+from pyaop import AOP, Proxy, Return
 
 LOG = logging.getLogger(__name__)
 
@@ -28,3 +31,52 @@ def time_profiler(func):
         lp.print_stats()
         return func_return
     return wrapper
+
+
+def proxy(obj, prop, prop_name):
+    """
+    为 object 对象代理一个属性
+    :param object obj: 被代理的对象
+    :param object prop: 代理返回的属性
+    :param str prop_name: 被代理的属性名
+    :return: 被代理之后的对象
+    :rtype: object
+    """
+    def common(proxy, name, value=None):
+        if name == prop_name:
+            Return(prop)
+
+    return Proxy(obj, before=[
+        AOP.Hook(common, ["__getattribute__", "__setattr__", "__delattr__"]),
+    ])
+
+
+class TimeProfilerMixin(object):
+    """
+    时间分析器 Mixin 类
+
+    用以提供复杂的时间分析功能，如：
+    - 针对需要多次调用的方法进行累加分析的场景
+    - 在一次代码执行流程中同时分析多个方法，并灵活控制分析结果的输出
+    """
+    _POOL = defaultdict(LineProfiler)  #: 用来暂存分析器的池子
+    time_profiler = None  # type: LineProfiler
+
+    @classmethod
+    @contextmanager
+    def get_time_profiler(cls, self_or_cls, **callargs):
+        """
+        获取时间分析器
+
+        :param object self_or_cls: 被代理的对象 or 类
+        :param dict callargs: 调用该上下文管理器时传入的所有调用参数
+        :return: 返回被代理的对象 or 类
+        :rtype: Iterator[object]
+        """
+        with super(TimeProfilerMixin, cls).get_time_profiler(
+                self_or_cls, **callargs) as self_or_cls:
+            time_profiler_name = callargs.get('_time_profiler_name', 'default')
+            yield proxy(
+                self_or_cls,
+                prop_name='time_profiler',
+                prop=cls._POOL[time_profiler_name])
