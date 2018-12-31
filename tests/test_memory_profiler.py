f0971fc22f2e82d4226491ca317071afbfe933e5
Moore.Huang <moore@moorehy.com>
2018-12-31 22:08:12 +0800

    Feature: 实现内存分析器的函数装饰器单元测试

---
+++ tests/test_memory_profiler.py
@@ -0,0 +1,27 @@
+# encoding=utf8
+"""
+测试内存分析器
+"""
+from moprofiler import memory_profiler
+
+
+@memory_profiler
+def calc_sum():
+    """求和计算"""
+    _sum = 0
+    for _item in range(10000):
+        _sum += 1
+    return _sum
+
+
+class TestMemoryProfiler(object):
+    """测试用于装饰函数的内存分析器"""
+
+    @staticmethod
+    def test_memory_profiler():
+        """测试内存分析器装饰器"""
+        print('求和结果: {}'.format(calc_sum()))
+
+
+if __name__ == '__main__':
+    print('求和结果: {}'.format(calc_sum()))
