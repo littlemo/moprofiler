a23f7bf5542fc413640597e87f485c3cb878a7be
Moore.Huang <moore@moorehy.com>
2019-01-03 17:10:30 +0800

    Feature: 将 stopwatch 与 StopwatchMixin 添加到包 __all__ 中

--- source/moprofiler/__init__.py
+++ source/moprofiler/__init__.py
@@ -3,8 +3,11 @@
 提供针对时间、内存的分析器，以及秒表日志打点工具
 """
 from .memory import MemoryProfilerMixin, memory_profiler
+from .stopwatch import StopwatchMixin, stopwatch
 from .time import TimeProfilerMixin, time_profiler
 
 __all__ = [
     'time_profiler', 'TimeProfilerMixin',
-    'memory_profiler', 'MemoryProfilerMixin']
+    'memory_profiler', 'MemoryProfilerMixin',
+    'stopwatch', 'StopwatchMixin',
+]
