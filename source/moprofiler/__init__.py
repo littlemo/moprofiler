256c49c4435c585d6760df69db35a8398a9fb68b
Moore.Huang <moore@moorehy.com>
2019-01-13 21:56:46 +0800

    Optimize: 根据重构后的分析器更新 init 中的 all

--- source/moprofiler/__init__.py
+++ source/moprofiler/__init__.py
@@ -2,12 +2,13 @@
 """
 提供针对时间、内存的分析器，以及秒表日志打点工具
 """
-from .memory import MemoryProfilerMixin, memory_profiler
+from .memory import (MemoryProfilerMixin, memory_profiler,
+                     memory_profiler_getter)
 from .stopwatch import StopwatchMixin, stopwatch
-from .time import TimeProfilerMixin, time_profiler
+from .time import TimeProfilerMixin, time_profiler, time_profiler_getter
 
 __all__ = [
-    'time_profiler', 'TimeProfilerMixin',
-    'memory_profiler', 'MemoryProfilerMixin',
+    'time_profiler', 'TimeProfilerMixin', 'time_profiler_getter',
+    'memory_profiler', 'MemoryProfilerMixin', 'memory_profiler_getter',
     'stopwatch', 'StopwatchMixin',
 ]
