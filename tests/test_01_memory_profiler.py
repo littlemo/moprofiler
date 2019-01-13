bae76a8bbc6225d059142172f9c34097f046f5c3
Moore.Huang <moore@moorehy.com>
2019-01-13 22:09:42 +0800

    Optimize: 完善内存的累加统计测试

--- tests/test_01_memory_profiler.py
+++ tests/test_01_memory_profiler.py
@@ -2,7 +2,9 @@
 """
 测试内存分析器
 """
-from moprofiler import memory_profiler
+import gc
+
+from moprofiler import memory_profiler, memory_profiler_getter
 
 
 @memory_profiler
@@ -10,6 +12,15 @@ def calc_sum():
     """求和计算"""
     _sum = 0
     for _item in range(10000):
+        _sum += 1
+    return _sum
+
+
+@memory_profiler(print_res=False)
+def calc_sum_2(x=10000):
+    """求和计算"""
+    _sum = 0
+    for _item in range(x):
         _sum += 1
     return _sum
 
@@ -22,6 +33,10 @@ class TestMemoryProfiler(object):
         """测试内存分析器装饰器"""
         # 此处由于内存计算处理的加载机制问题，在单测中无法正常统计到
         print('求和结果: {}'.format(calc_sum()))
+        gc.collect()
+        calc_sum_2(10000)
+        calc_sum_2(10000)
+        memory_profiler_getter('calc_sum_2').print_stats()
 
 
 if __name__ == '__main__':
