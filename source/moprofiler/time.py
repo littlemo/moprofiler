f12935263a5793533d544697d2d323e1ef9aab40
Moore.Huang <moore@moorehy.com>
2019-02-14 19:59:11 +0800

    Feature: 将原先的函数装饰器修改为类装饰器，从而简化设计

--- source/moprofiler/time.py
+++ source/moprofiler/time.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 import logging
 import types  # pylint: disable=W0611
 from collections import defaultdict
-from functools import wraps
+from functools import update_wrapper
 
 from line_profiler import LineProfiler
 
@@ -75,70 +75,88 @@ class TimeProfilerMixin(base.ProfilerMixin):
             name, raise_except=raise_except, force_new_profiler=force_new_profiler)
 
 
-def time_profiler(
-        _function=None, name='', print_res=True,
-        stream=None, output_unit=None, stripzeros=False,
-        force_new_profiler=False):
-    """
-    时间分析器装饰器
-
-    逐行分析被装饰函数每行的执行时间
-
-    :param _function: 被封装的对象，由解释器自动传入，不需关心
-    :type _function: types.FunctionType or types.MethodType
-    :param str name: 关键字参数，被装饰方法所使用的时间分析器名称，默认为使用被装饰方法的方法名
-    :param bool print_res: 是否在被装饰对象退出后立刻打印分析结果，默认为 True 。
-        当需要将多次调用结果聚集后输出时，可设为 False ，并通过 Mixin 中的 time_profiler 进行结果输出
-    :param object stream: 输出方式，默认为 stdout ，可指定为文件
-    :param str output_unit: 输出单位
-    :param bool stripzeros: 是否去零
-    :param bool force_new_profiler: 是否强制使用新的分析器，默认为 ``否``
-    :return: 装饰后的函数或方法
-    :rtype: types.FunctionType or types.MethodType
-    """
-    invoked = bool(_function and callable(_function))
-    if invoked:
-        func = _function  # type: types.FunctionType or types.MethodType
-
-    def wrapper(func):
+class time_profiler(object):  # pylint: disable=R0902
+    """时间分析器"""
+    def __init__(
+            self, _function=None, name='', print_res=True,
+            stream=None, output_unit=None, stripzeros=False,
+            force_new_profiler=False):
         """
-        装饰器封装函数
+        时间分析器装饰器
+
+        逐行分析被装饰函数每行的执行时间
+
+        :param _function: 被封装的对象，由解释器自动传入，不需关心
+        :type _function: types.FunctionType or types.MethodType
+        :param str name: 关键字参数，被装饰方法所使用的时间分析器名称，默认为使用被装饰方法的方法名
+        :param bool print_res: 是否在被装饰对象退出后立刻打印分析结果，默认为 True 。
+            当需要将多次调用结果聚集后输出时，可设为 False ，并通过 Mixin 中的 time_profiler 进行结果输出
+        :param object stream: 输出方式，默认为 stdout ，可指定为文件
+        :param str output_unit: 输出单位
+        :param bool stripzeros: 是否去零
+        :param bool force_new_profiler: 是否强制使用新的分析器，默认为 ``否``
+        :return: 装饰后的函数或方法
+        :rtype: types.FunctionType or types.MethodType
         """
-        class inner(object):
-            """类封装装饰器"""
-            def __init__(self):
-                self.time_profiler = None
+        self._func = None
 
-            def init_time_profiler(self, *args):
-                """
-                初始化时间分析器
-                """
-                _name = name or func.__name__
-                if not (args and base.is_instance_or_subclass(args[0], TimeProfilerMixin)):
-                    # 若当前被装饰的方法未继承 TimeProfilerMixin ，则将其作为普通函数装饰
-                    lp = time_profiler_getter(
-                        _name, raise_except=False, force_new_profiler=force_new_profiler)
-                else:
-                    self_or_cls = args[0]  # type: TimeProfilerMixin
-                    lp = self_or_cls.time_profiler(
-                        _name, raise_except=False, force_new_profiler=force_new_profiler)
-                self.time_profiler = lp
+        _invoked = bool(_function and callable(_function))
+        self.func = _function if _invoked \
+            else None  # type: types.FunctionType or types.MethodType
+        self.time_profiler = None
 
-            @wraps(func)
-            def __call__(self, *args, **kwargs):
-                """
-                将被封装方法使用 LineProfiler 进行封装
-                """
-                if not self.time_profiler:
-                    self.init_time_profiler(*args)
-                profiler_wrapper = self.time_profiler(func)
-                res = profiler_wrapper(*args, **kwargs)
-                if print_res:  # pragma: no cover
-                    # 此处由于 LineProfiler 的 C 库造成的 coverage 统计 Bug ，故手动配置为 no cover
-                    self.time_profiler.print_stats(
-                        stream=stream,
-                        output_unit=output_unit,
-                        stripzeros=stripzeros)
-                return res  # pragma: no cover
-        return inner()
-    return wrapper if not invoked else wrapper(func)
+        self.name = name
+        self.print_res = print_res
+        self.stream = stream
+        self.output_unit = output_unit
+        self.stripzeros = stripzeros
+        self.force_new_profiler = force_new_profiler
+
+    @property
+    def func(self):
+        """被封装函数的 getter 方法"""
+        return self._func
+
+    @func.setter
+    def func(self, func):
+        """被封装函数的 setter 方法"""
+        if func:
+            update_wrapper(self, func)
+        self._func = func
+
+    def init_time_profiler(self, *args):
+        """
+        初始化时间分析器
+        """
+        _name = self.name or self.func.__name__
+        if not (args and base.is_instance_or_subclass(args[0], TimeProfilerMixin)):
+            # 若当前被装饰的方法未继承 TimeProfilerMixin ，则将其作为普通函数装饰
+            lp = time_profiler_getter(
+                _name, raise_except=False, force_new_profiler=self.force_new_profiler)
+        else:
+            self_or_cls = args[0]  # type: TimeProfilerMixin
+            lp = self_or_cls.time_profiler(
+                _name, raise_except=False, force_new_profiler=self.force_new_profiler)
+        self.time_profiler = lp
+
+    def __call__(self, *args, **kwargs):
+        _func = self.func
+        if not self.func:
+            self.func = args[0]
+        return self if not _func else self._wrapper(*args, **kwargs)
+
+    def _wrapper(self, *args, **kwargs):
+        """
+        将被封装方法使用 LineProfiler 进行封装
+        """
+        if not self.time_profiler:
+            self.init_time_profiler(*args)
+        profiler_wrapper = self.time_profiler(self.func)
+        res = profiler_wrapper(*args, **kwargs)
+        if self.print_res:  # pragma: no cover
+            # 此处由于 LineProfiler 的 C 库造成的 coverage 统计 Bug ，故手动配置为 no cover
+            self.time_profiler.print_stats(
+                stream=self.stream,
+                output_unit=self.output_unit,
+                stripzeros=self.stripzeros)
+        return res  # pragma: no cover
--- tests/test_50_time_profiler.py
+++ tests/test_50_time_profiler.py
@@ -4,43 +4,60 @@
 """
 import random
 
-from moprofiler import time, time_profiler, time_profiler_getter
+from moprofiler import time_profiler
+
+
+@time_profiler
+def _judge_with_set(x):
+    """test_wrapper_set_docstring"""
+    cnt = 0
+    tp = {'1', '3', '5', '7', '11'}
+    for i in x:
+        if i in tp:
+            cnt += 1
+    return cnt
+
+@time_profiler(print_res=False)
+def _judge_with_list(x):
+    """test_wrapper_list_docstring"""
+    cnt = 0
+    tp = ['1', '3', '5', '7', '11']
+    for i in x:
+        if i in tp:
+            cnt += 1
+    return cnt
 
 
 class TestTimeProfiler(object):
     """测试用于装饰函数的时间分析器"""
 
     @staticmethod
-    def test_time_profiler():
-        """测试时间分析器装饰器"""
-
-        @time_profiler
-        def _judge_with_set(x):
-            cnt = 0
-            tp = {'1', '3', '5', '7', '11'}
-            for i in x:
-                if i in tp:
-                    cnt += 1
-            return cnt
-
-        @time_profiler(print_res=False)
-        def _judge_with_list(x):
-            cnt = 0
-            tp = ['1', '3', '5', '7', '11']
-            for i in x:
-                if i in tp:
-                    cnt += 1
-            return cnt
+    def test_time_profiler_run():
+        """测试时间分析器装饰器执行"""
+        assert _judge_with_set.time_profiler is None
+        assert _judge_with_list.time_profiler is None
 
         random_list = [random.choice(['2', '11']) for _x in range(1000)]
+        _judge_with_set(random_list)
         assert _judge_with_set(random_list) == _judge_with_list(random_list)
-        _judge_with_list(random_list)
-        print('时间分析器暂存池：{}'.format(getattr(time, '__time_profiler_pool').keys()))
+
         p1 = _judge_with_list.time_profiler
         p1.print_stats()
         _judge_with_list(random_list)
         p2 = _judge_with_list.time_profiler
         assert p1 is p2
+
+    @staticmethod
+    def test_time_profiler_wraps():
+        """测试时间分析器的名称&文档串替换"""
+        assert _judge_with_set.__name__ == '_judge_with_set'
+        assert _judge_with_list.__name__ == '_judge_with_list'
+
+        assert _judge_with_set.__doc__ == 'test_wrapper_set_docstring'
+        assert _judge_with_list.__doc__ == 'test_wrapper_list_docstring'
+
+        assert isinstance(_judge_with_set, time_profiler)
+        assert isinstance(_judge_with_list, time_profiler)
 
     @staticmethod
     def test_time_profiler_with_force_new_profiler():
@@ -51,7 +68,7 @@ class TestTimeProfiler(object):
             pass
 
         _force_new_profiler()
-        p1 = time_profiler_getter('_force_new_profiler')
+        p1 = _force_new_profiler.time_profiler
         _force_new_profiler()
-        p2 = time_profiler_getter('_force_new_profiler')
+        p2 = _force_new_profiler.time_profiler
         assert p1 is not p2
