a9ffce118d4516fe80de0d916f701ec80f91162f
Moore.Huang <moore@moorehy.com>
2019-02-15 23:06:32 +0800

    Optimize: 完善重构后未更新的参数文档说明

--- source/moprofiler/memory.py
+++ source/moprofiler/memory.py
@@ -60,13 +60,14 @@ class MemoryProfiler(base.ProfilerClassDecorator):
             self, _function=None, print_res=True, stream=None,
             precision=1, backend='psutil', **kwargs):
         """
-        内存分析器装饰器
+        内存分析器的类装饰器
 
         :param _function: 被封装的对象，由解释器自动传入，不需关心
         :type _function: types.FunctionType or types.MethodType
-        :param bool print_res: 是否在被装饰对象退出后立刻打印分析结果，默认为 True 。
-            当需要将多次调用结果聚集后输出时，可设为 False ，
-            并通过 Mixin 中的 memory_profiler 进行结果输出
+        :param bool print_res: 是否在被装饰对象退出后立刻打印分析结果，默认为 ``True`` 。
+            当需要将多次调用结果聚集后输出时，可设为 ``False`` ，并通过调用被装饰函数/方法
+            （装饰后将被替换为 :py:class:`~moprofiler.memory.MemoryProfiler` ）的
+            :py:meth:`~moprofiler.memory.MemoryProfiler.print_stats` 方法进行结果输出
         :param object stream: 输出方式，默认为 stdout ，可指定为文件
         :param int precision: 精度，默认为 1
         :param str backend: 内存监控的 backend ，默认为 'psutil'
--- source/moprofiler/time.py
+++ source/moprofiler/time.py
@@ -22,14 +22,16 @@ class TimeProfiler(base.ProfilerClassDecorator):
             self, _function=None, print_res=True, stream=None,
             output_unit=None, stripzeros=False, **kwargs):
         """
-        时间分析器装饰器
+        时间分析器的类装饰器
 
         逐行分析被装饰函数每行的执行时间
 
         :param _function: 被封装的对象，由解释器自动传入，不需关心
         :type _function: types.FunctionType or types.MethodType
-        :param bool print_res: 是否在被装饰对象退出后立刻打印分析结果，默认为 True 。
-            当需要将多次调用结果聚集后输出时，可设为 False ，并通过 Mixin 中的 time_profiler 进行结果输出
+        :param bool print_res: 是否在被装饰对象退出后立刻打印分析结果，默认为 ``True`` 。
+            当需要将多次调用结果聚集后输出时，可设为 ``False`` ，并通过调用被装饰函数/方法
+            （装饰后将被替换为 :py:class:`~moprofiler.time.TimeProfiler` ）的
+            :py:meth:`~moprofiler.time.TimeProfiler.print_stats` 方法进行结果输出
         :param object stream: 输出方式，默认为 stdout ，可指定为文件
         :param str output_unit: 输出单位
         :param bool stripzeros: 是否去零
