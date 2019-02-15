7b563aff13215605d2181abebf57cc6df944224b
Moore.Huang <moore@moorehy.com>
2019-02-15 15:26:36 +0800

    Modify: 整理 __init__ 中关于内存分析器装饰器的定义

--- source/moprofiler/__init__.py
+++ source/moprofiler/__init__.py
@@ -2,13 +2,13 @@
 """
 提供针对时间、内存的分析器，以及秒表日志打点工具
 """
-from .memory import (MemoryProfilerMixin, memory_profiler,
-                     memory_profiler_getter)
+from .memory import MemoryProfiler, memory_profiler
 from .stopwatch import StopwatchMixin, stopwatch
 from .time import TimeProfiler, time_profiler
 
 __all__ = [
-    'TimeProfiler', 'time_profiler', 'memory_profiler',
-    'MemoryProfilerMixin', 'memory_profiler_getter',
-    'stopwatch', 'StopwatchMixin',
+    'TimeProfiler', 'MemoryProfiler',
+    'StopwatchMixin', 'stopwatch',
+
+    'time_profiler', 'memory_profiler',  # 这两个别名用于向后兼容，后续版本将删除
 ]
