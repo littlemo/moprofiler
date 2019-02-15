51feb1ff519de80b45708fdcc8927aa9a7f90ea2
Moore.Huang <moore@moorehy.com>
2019-02-15 16:06:16 +0800

    Feature: 根据重构后的内存分析器更新用于测试函数装饰的单元测试

--- tests/test_01_memory_profiler_to_function.py
+++ tests/test_01_memory_profiler_to_function.py
@@ -4,10 +4,12 @@
 """
 import gc
 
-from moprofiler import memory_profiler, memory_profiler_getter
+from memory_profiler import LineProfiler
+
+from moprofiler import MemoryProfiler
 
 
-@memory_profiler
+@MemoryProfiler
 def calc_sum():
     """求和计算"""
     _sum = 0
@@ -16,7 +18,7 @@ def calc_sum():
     return _sum
 
 
-@memory_profiler(print_res=False)
+@MemoryProfiler(print_res=False, fake_method=False)
 def calc_sum_2(x=10000):
     """求和计算"""
     _sum = 0
@@ -25,36 +27,56 @@ def calc_sum_2(x=10000):
     return _sum
 
 
-class TestMemoryProfiler(object):
-    """测试用于装饰函数的内存分析器"""
+class TestMemoryProfilerToFunction(object):
+    """测试用于装饰函数的内存分析器类装饰器"""
 
     @staticmethod
-    def test_memory_profiler():
+    def test_memory_profiler_call():
         """测试内存分析器装饰器"""
         # 此处由于内存计算处理的加载机制问题，在单测中无法正常统计到
+        assert isinstance(calc_sum.profiler, LineProfiler)
+        assert isinstance(calc_sum_2.profiler, LineProfiler)
+
         print('求和结果: {}'.format(calc_sum()))
         gc.collect()
         calc_sum_2(10000)
-        p1 = memory_profiler_getter('calc_sum_2')
+
+        assert isinstance(calc_sum.profiler, LineProfiler)
+        assert isinstance(calc_sum_2.profiler, LineProfiler)
+        assert calc_sum.profiler is not calc_sum_2.profiler
+
+        p1 = calc_sum_2.profiler
         calc_sum_2(10000)
-        p2 = memory_profiler_getter('calc_sum_2')
+        p2 = calc_sum_2.profiler
         p2.print_stats()
         assert p1 is p2
+
+    @staticmethod
+    def test_memory_profiler_wraps():
+        """测试内存分析器的名称&文档串替换"""
+        assert calc_sum.__name__ == 'calc_sum'
+        assert calc_sum_2.__name__ == 'calc_sum_2'
+
+        assert calc_sum.__doc__ == '求和计算'
+        assert calc_sum_2.__doc__ == '求和计算'
+
+        assert isinstance(calc_sum, MemoryProfiler)
+        assert isinstance(calc_sum_2, MemoryProfiler)
 
     @staticmethod
     def test_memory_profiler_with_force_new_profiler():
         """测试强制使用新分析器的参数功能"""
 
-        @memory_profiler(print_res=False, force_new_profiler=True)
+        @MemoryProfiler(print_res=False, force_new_profiler=True)
         def _force_new_profiler():
             pass
 
         _force_new_profiler()
-        p1 = memory_profiler_getter('_force_new_profiler')
+        p1 = _force_new_profiler.profiler
         _force_new_profiler()
-        p2 = memory_profiler_getter('_force_new_profiler')
+        p2 = _force_new_profiler.profiler
         assert p1 is not p2
 
 
 if __name__ == '__main__':
-    TestMemoryProfiler.test_memory_profiler()
+    TestMemoryProfilerToFunction.test_memory_profiler_call()
