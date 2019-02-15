77a99c7860b6296f3f361f995487b2b5464a3f1b
Moore.Huang <moore@moorehy.com>
2019-02-15 21:22:51 +0800

    Feature: 根据重构后的时间&内存分析器更新混用分析器的单元测试

--- tests/test_03_multi_mixin.py
+++ tests/test_03_multi_mixin.py
@@ -1,31 +1,28 @@
 # encoding=utf8
 """
-测试时间&内存分析器 Mixin 的共用
+测试时间&内存分析器与秒表的共用
 """
 import time
 
-import pytest
 from line_profiler import LineProfiler as TimeLineProfiler
+from memory_profiler import LineProfiler as MemoryLineProfiler
 
-from moprofiler import (MemoryProfilerMixin, StopwatchMixin, TimeProfilerMixin,
-                        memory, memory_profiler, stopwatch)
-from moprofiler import time as ptime
-from moprofiler import time_profiler
+from moprofiler import MemoryProfiler, StopwatchMixin, TimeProfiler, stopwatch
 
 
-class MultiMixin(MemoryProfilerMixin, TimeProfilerMixin, StopwatchMixin):
+class MultiMixin(StopwatchMixin):
     """
-    混合 Mixin
+    混合使用两种分析器&秒表
     """
     # 不支持同时监控一个方法的时间&内存，最终会变成外层装饰器监控内层装饰器中的代码
-    @memory_profiler(name='wuwuwu')
+    @MemoryProfiler
     def list_waste(self):  # pylint: disable=R0201
         """列表"""
         a = [1] * (10 ** 5)
         return a
 
     @classmethod
-    @time_profiler
+    @TimeProfiler
     def dict_waste(cls, a):
         """字典"""
         ret = {}
@@ -43,23 +40,18 @@ class MultiMixin(MemoryProfilerMixin, TimeProfilerMixin, StopwatchMixin):
 
 
 class TestMultiMixin(object):
-    """测试用于装饰方法的内存分析器"""
+    """测试用于装饰类中多个方法的多种分析器&秒表"""
 
     @staticmethod
     def test_multi_mixin():
-        """测试内存分析器的 mixin"""
+        """测试多分析器&秒表共用"""
         mm = MultiMixin()
         x = mm.list_waste()
         mm.dict_waste(x)
         mm.orz_instancemethod(5)
-        print('\n时间分析器暂存池：{}'.format(getattr(ptime, '__time_profiler_pool').keys()))
-        print('内存分析器暂存池：{}'.format(getattr(memory, '__memory_profiler_pool').keys()))
-        with pytest.raises(KeyError):
-            mm.memory_profiler('list_waste')
-        assert isinstance(mm.time_profiler('dict_waste'), TimeLineProfiler)
 
-        mm.memory_profiler('wuwuwu').print_stats()
-        mm.time_profiler('dict_waste').print_stats()
+        assert isinstance(mm.dict_waste.profiler, TimeLineProfiler)
+        assert isinstance(mm.list_waste.profiler, MemoryLineProfiler)
 
 
 if __name__ == '__main__':
